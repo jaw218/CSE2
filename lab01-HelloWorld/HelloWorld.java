7c757460afc943f0809f3d12deff1ad262e71786
jaw218 <jaw218@lehigh.edu>
2015-01-23 15:37:19 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,30 @@
+//
+//James Waring
+//Demo0
+//Hellow World Java Program
+//
+//  first compile the program
+//      javac HelloWorld.java
+//  run the program
+//      jave HelloWorld//
+
+//define a class
+public class HelloWorld
+{
+    
+    //add a main method
+        public static void main(String[] args)
+        {
+            //prints the statement below
+            System.out.println("######################");
+            System.out.println("######################");
+            System.out.println("#                    #");
+            System.out.println("#  Hello World 2015  #");
+            System.out.println("#                    #");
+            System.out.println("######################");
+            System.out.println("######################");
+            System.out.println("test");
+            
+            //test/testtest
+        }
+}
