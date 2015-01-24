17c01547ae5f52b50c7c3d2375037996285e0aa4
jaw218 <jaw218@lehigh.edu>
2015-01-24 00:03:23 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,25 @@
+//
+// James Waring
+//
+// Hw 2 Welcome Class
+//
+// define a class
+public class WelcomeClass
+{
+    //create a main method
+    public static void main(String[] args)
+    {
+        
+        //Prints out the qouted statements
+        System.out.println("  -------------");
+        System.out.println("  |  WELCOME  |");
+        System.out.println("  -------------");
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+        System.out.println("<-J--A--W--2--0--8->");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("  v  v  v  v  v  v");
+        System.out.println("I enjoy long walks on the beach; romantic, candle-lit dinners;");
+        System.out.println("cooking; free-verse poetry slams; and intimate cuddling.");
+    }
+}
