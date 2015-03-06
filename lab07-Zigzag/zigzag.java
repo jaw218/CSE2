9860b097ce5a4f0943bad4bb984df3038f6848fb
jaw218 <jaw218@lehigh.edu>
2015-03-06 14:38:55 +0000

    Updated Lab07

--- lab07-Zigzag/zigzag.java
+++ lab07-Zigzag/zigzag.java
@@ -7,26 +7,87 @@ public class zigzag
 {
     public static void main(String[] args)
     {
-        int nStars = 0;
-        boolean notDone = true;
-        String garbage;
-        double l;
-        System.out.print("Please enter an integer between 3 and 33 inclusively: ");
-        Scanner myScanner = new Scanner(System.in);
-        while(notDone)
+        boolean cont = true;
+        while(cont)
         {
-            if(myScanner.hasNextDouble())
-                {
-                    l = myScanner.nextDouble();
-                    
-                    //checks to see if non-negative and int
-                    if((l >= 3) && (l%1 == 0) && (l <=33))
+            int nStars = 0;
+            boolean notDone = true;
+            String garbage;
+            double l;
+            System.out.print("Please enter an integer between 3 and 33 inclusively: ");
+            Scanner myScanner = new Scanner(System.in);
+            while(notDone)
+            {
+                if(myScanner.hasNextDouble())
                     {
-                        nStars = (int)l;
-                        notDone = false;
+                        l = myScanner.nextDouble();
+                    
+                        //checks to see if non-negative and int
+                        if((l >= 3) && (l%1 == 0) && (l <=33))
+                        {
+                            nStars = (int)l;
+                            notDone = false;
+                        }
+                        else
+                        {   
+                        System.out.println("Not valid.");
+                        }
                     }
                     else
                     {
+                        System.out.println("Not valid.");
+                        garbage = myScanner.next();
+                    }
+                }
+            int x = 0;
+            int y = 0;
+            while(x < nStars)
+            {
+                System.out.print("*");
+                x++;
+            }
+            System.out.println();
+            x = 0;
+            while(x < nStars)
+            {
+                while(y < x)
+                {
+                    System.out.print(" ");
+                    y++;
+                }
+                System.out.println("*");
+                y = 0;
+                x++;
+            }
+            x = 0;
+            while(x < nStars)
+            {
+                System.out.print("*");
+                x++;
+            }
+            System.out.println();
+            System.out.println("Do you want to go again? (Y/N)");
+            boolean notDone2 = true;
+            String m;
+            while(notDone2)
+            {
+                if(myScanner.hasNext())
+                {
+                    m = myScanner.next();
+                
+                    //checks to see if non-negative and int
+                    if((m.equals("Y")) || (m.equals("y")))
+                    {
+                        cont = true;
+                        notDone2 = false;
+                    }
+                    else if((m.equals("N")) || (m.equals("n")))
+                    {
+                        cont = false;
+                        notDone2 = false;
+                    }
+                    else
+                    {   
                         System.out.println("Not valid.");
                     }
                 }
@@ -36,32 +97,6 @@ public class zigzag
                     garbage = myScanner.next();
                 }
             }
-        int x = 0;
-        int y = 0;
-        while(x < nStars)
-        {
-            System.out.print("*");
-            x++;
         }
-        System.out.println();
-        x = 0;
-        while(x < nStars)
-        {
-            while(y < x)
-            {
-                System.out.print(" ");
-                y++;
-            }
-            System.out.println("*");
-            y = 0;
-            x++;
-        }
-        x = 0;
-        while(x < nStars)
-        {
-            System.out.print("*");
-            x++;
-        }
-        System.out.println();
     }
 }
