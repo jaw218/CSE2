b207805f628ad0f1f94659909674109bbefa1fcb
jaw218 <jaw218@lehigh.edu>
2015-03-19 21:50:18 +0000

    Updated hw08

--- hw08/HW8.java
+++ hw08/HW8.java
@@ -2,10 +2,11 @@
 // 3/19/15
 // Hw 08 Game
 
-import java.util.Scanner
+import java.util.Scanner;
 
 public class HW8
 {
+    //main method, runs entire game
     public static void main(String[] args) 
     {
         char option;
@@ -28,25 +29,167 @@ public class HW8
         System.out.println("Enter 'A' or 'a' to Attack, 'E' or 'E' to Escape, ANYTHING else is to YIELD");
         input = getInput(scan, "AaEe", 10);
         System.out.println("Congratulations! You SURVIVED! Get your REWARD!");
-        System.out.println("There are three 3 tressure box in front of you! Enter the box number you want to open!");
+        System.out.println("There are three 3 treasure boxes in front of you! Enter the box number you want to open!");
         box();
         input = getInput(scan);
         System.out.println("Hero! Have a good day!");
     }
     
-    public static void cave()
+    public static String getInput(Scanner scan, String string)
     {
-        System.out.println("                              *****   * * * * * * * *        ");
-        System.out.println("                         ***         ***                      ");
-        System.out.println("                      ***               ***                   ");
-        System.out.println("                 |    ***               ***                   ");
-        System.out.println("                 |    ***               ***                   ");
-        System.out.println("             O __|__  ***               ***                   ");
-        System.out.println("           ******l    ***               ***                   ");
-        System.out.println("            * *       ***               ***                   ");
-        System.out.println("           *   *      ********************* * * * * * *       ");
+        String x;
+        //checks conditions to take appropriate action based on user input
+        if(scan.hasNext())
+        {
+            String in = scan.next();
+            if((in.equals("c")) || (in.equals("C")))
+            {
+                x = "";
+            }
+            else
+            {
+                x = "Yeah right LOSER";
+                System.out.println(x);
+                System.exit(0);
+            }
+        }
+        else
+        {
+            x = "Yeah right LOSER";
+            System.out.println(x);
+            System.exit(0);
+        }
+        return x;
     }
     
+    public static String getInput(Scanner scan, String string, int trial)
+    {
+        String result = "";
+        int x = trial;
+        //runs until 10 crit hits or escape or quit
+        while(x > 0)
+        {
+            //checks for char and then takes appropriate action for input
+            if(scan.hasNext())
+            {
+                String in = scan.next();
+                if((in.equals("a")) || (in.equals("A")))
+                {
+                    int y = (int)(Math.random() * 2);
+                    if(y == 1)
+                    {
+                        x--;
+                        System.out.println("Critical Hit!!!");
+                    }
+                    else
+                    {
+                        System.out.println("You missed.");
+                    }
+                }
+                else if((in.equals("e")) || (in.equals("E")))
+                {
+                    int y = (int)(Math.random() * 10 + 1);
+                    if(y == 10)
+                    {
+                        x = 0;
+                        System.out.println("You escaped!");
+                    }
+                    else
+                    {
+                        System.out.println("You couldn't escape.");
+                    }
+                }
+                else
+                {
+                    result= "You were executed by the giant.";
+                    System.out.println(result);
+                    System.exit(0);
+                }
+            }
+            else
+            {
+                result = "You were executed by the giant.";
+                System.exit(0);
+            }
+            if(x>0)
+            {
+                System.out.println("Enter 'A' or 'a' to Attack, 'E' or 'E' to Escape, ANYTHING else is to YIELD");
+            }
+        }
+        return result;
+    }
+    
+    public static String getInput(Scanner scan)
+    {
+        double l = 0;
+        String garbage;
+        String ugh;
+        int choice = 0;
+        boolean notDone = true;
+        while(notDone)
+        {
+            if(scan.hasNextDouble())
+            {
+                l = scan.nextDouble();
+                    
+                //checks to see if valid
+                if(((l == 1) || (l == 2) || (l == 3)) && (l%1 == 0))
+                {
+                    choice = (int)l;
+                    notDone = false;
+                    
+                }
+                else
+                {   
+                    choice = 5;
+                    notDone = false;
+                }
+            }
+            else
+            {
+                choice = 5;
+                garbage = scan.next();
+                notDone = false;
+            }
+        }
+        //switches for choice of box
+        switch(choice)
+        {
+            case 1:
+                System.out.println("You received an A in CSE 002.");
+                ugh = "I dont see the point of these returns if we never use the variable input";
+                break;
+            case 2:
+                System.out.println("You received 50 Dining Dollars.");
+                ugh = "I dont see the point of these returns if we never use the variable input";
+                break;
+            case 3:
+                System.out.println("You received a Ferrari.");
+                ugh = "I dont see the point of these returns if we never use the variable input";
+                break;
+            default:
+                System.out.println("A Wrong Number! You get nothing! Better restart the game LOL");
+                ugh = "seriously...";
+        }
+        return ugh;
+    }
+    
+    //prints cave
+    public static void cave()
+    {
+        System.out.println("                              *****   * * * * * * * *");
+        System.out.println("                        ***          ***");
+        System.out.println("                     ***                ***");
+        System.out.println("                 |   ***                ***");
+        System.out.println("                 |   ***                ***");
+        System.out.println("             O __|__ ***                ***");
+        System.out.println("           ******l   ***                ***");
+        System.out.println("            * *      ***                ***");
+        System.out.println("           *   *     ********************** * * * * * *");
+    }
+   
+    
+    //prints giant
     public static void giant()
     {
         System.out.println("                                 ---------                    ");
@@ -61,6 +204,7 @@ public class HW8
         System.out.println("                               HHH       HHH                   ");
     }
     
+    //prints boxes
     public static void box()
     {
         System.out.println("                     *********************     *********************    *********************             ");
